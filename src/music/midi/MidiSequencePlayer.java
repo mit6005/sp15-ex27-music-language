6e1d53769d88ddb8ea20fda529df6954cc77bff3
Max Goldman <maxg@mit.edu>
2015-04-27 09:47:42 -0400

    The rest of the story

--- src/music/midi/MidiSequencePlayer.java
+++ src/music/midi/MidiSequencePlayer.java
@@ -136,6 +136,7 @@ public class MidiSequencePlayer implements SequencePlayer {
     /**
      * Schedule a callback when the synthesizer reaches a time.
      */
+    @Override
     public void addEvent(Consumer<Double> callback, double atBeat) {
         int callbackNumber = saveCallback(callback);
         try {
